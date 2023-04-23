5ee1c82eedd291c46f1ff94ed81670d2767bd301
Sabs112 <12100531@students.koi.edu.au>
2023-04-23 23:18:47 +1000

    Additional

--- MyConsole/MyConsole/Program.cs
+++ MyConsole/MyConsole/Program.cs
@@ -4,6 +4,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello there, World!");
-    }
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }  
+
+
 }
 
